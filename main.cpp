4488231b3602ffcf391133cd01bf50cfe9c53d84
Békési Dániel <bekesi.daniel@hotmail.hu>
2021-03-09 19:29:36 +0100

    Thread pool.

--- main.cpp
+++ main.cpp
@@ -2,22 +2,28 @@
 #include "SDL.h"
 #include <thread>
 #include <mutex>
+#include <vector>
+#include <queue>
+#include <functional>
 
-#define SCREEN_WIDTH 1024
-#define SCREEN_HEIGHT 768
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
 #define NUMBER_OF_THREADS 4
 
 std::mutex buffer_mutex;
+std::mutex Queue_Mutex;
+std::condition_variable condition;
+
+std::queue<std::function<void()>> Queue;
+
+Uint8 image[SCREEN_WIDTH * SCREEN_HEIGHT * 4];
+
 
 void drawAllPixels(SDL_Renderer* renderer, int thread_no) {
 	int upper_limit = (SCREEN_WIDTH / NUMBER_OF_THREADS) * thread_no;
 	int lower_limit = upper_limit - (SCREEN_WIDTH / NUMBER_OF_THREADS);
-
-	std::cout << "Calling thread " << thread_no << std::endl;
-	std::cout << "Lower " << lower_limit << std::endl;
-	std::cout << "Upper " << upper_limit << std::endl;
-
-
+ 
 	for (int i = lower_limit; i < upper_limit; i++) {
 		for (int j = 0; j < SCREEN_HEIGHT; j++) {
 			double x0 = i / (SCREEN_WIDTH / 3.5) - 2.5;
@@ -32,26 +38,9 @@ void drawAllPixels(SDL_Renderer* renderer, int thread_no) {
 				x = xtemp;
 				iteration = iteration + 1;
 			}
-			
-			std::lock_guard<std::mutex> guard(buffer_mutex);
-			SDL_SetRenderDrawColor(renderer, 0, 0, iteration * 2.5, 255);
-			SDL_RenderDrawPoint(renderer, i, j);
+			image[800 * 4 * j + i * 4 + 2] = iteration * 2.5;
 		}
 	}
-}
-
-void spawnThreads(SDL_Renderer* renderer) {
-	std::thread threads[NUMBER_OF_THREADS];
-
-	for (int i = 0; i < NUMBER_OF_THREADS; i++) {
-		threads[i] = std::thread(drawAllPixels, renderer, i + 1);
-	}
-
-	for (int i = 0; i < NUMBER_OF_THREADS; i++) {
-		threads[i].join();
-	}
-
-	std::cout << "Threads are done." << std::endl;
 }
 
 
@@ -64,18 +53,68 @@ bool OnEvent(SDL_Event* event) {
 	}
 }
 
+void add(std::function<void()> new_job) {
+	{
+		std::unique_lock<std::mutex> lock(Queue_Mutex);
+		Queue.push(new_job);
+	}
+	condition.notify_one();
+}
+
+void Infinite_loop_function() {
+	//std::cout << "in infinite loop function" << std::endl;
+	std::function<void()>  Job;
+	while (true)
+	{
+		{
+			std::unique_lock<std::mutex> lock(Queue_Mutex);
+
+			condition.wait(lock, [] { return !Queue.empty(); });
+			Job = Queue.front();
+			Queue.pop();
+		}
+		Job(); // function<void()> type
+	}
+}
+
+void initThreads(std::thread pool[NUMBER_OF_THREADS]) {
+	for (int i = 0; i < NUMBER_OF_THREADS; i++) {
+		pool[i] = std::thread(Infinite_loop_function);
+	}
+
+}
+
+void spawnThreads(SDL_Renderer* renderer) {
+	std::thread threads[NUMBER_OF_THREADS];
+
+	for (int i = 0; i < NUMBER_OF_THREADS; i++) {
+		add(std::bind(&drawAllPixels, renderer, i + 1));
+	}
+}
+
+
+
 int main(int argc, char* args[]) {
 	
 	bool running = true;
 	SDL_Window* window ;
 	SDL_Renderer* renderer;
 	SDL_Event Event;
+	SDL_Texture* texture = NULL;
 
 	SDL_Init(SDL_INIT_VIDEO);
-	SDL_CreateWindowAndRenderer(1024, 768, 0, &window, &renderer);
+	SDL_CreateWindowAndRenderer(SCREEN_WIDTH, SCREEN_HEIGHT, 0, &window, &renderer);
 
 	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 	SDL_RenderClear(renderer);
+
+	Uint64 NOW = SDL_GetPerformanceCounter();
+	Uint64 LAST = 0;
+	double deltaTime = 0;
+
+	std::thread thread_pool[NUMBER_OF_THREADS];
+	initThreads(thread_pool);
+
 
 	while (running)
 	{
@@ -86,12 +125,26 @@ int main(int argc, char* args[]) {
 		}
 
 		spawnThreads(renderer);
+
+		texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA32, SDL_TEXTUREACCESS_STATIC, 800, 600);
+		SDL_UpdateTexture(texture, NULL, image, 800 * 4);
+		SDL_RenderCopy(renderer, texture, NULL, NULL);
+
 		SDL_RenderPresent(renderer);
 
+		SDL_DestroyTexture(texture);
+
+		LAST = NOW;
+		NOW = SDL_GetPerformanceCounter();
+
+		deltaTime = (double)((NOW - LAST) * 1000 / (double)SDL_GetPerformanceFrequency());
+
+		std::cout << deltaTime << std::endl;
 	}
 
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
+
 	return 0;
 }
